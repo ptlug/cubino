49d173e5f6455aed98c40115be485c7b053f88e6
Andrea Stagi <stagi.andrea@gmail.com>
2013-10-29 04:01:55 +0100

    Added config and translations

---
+++ src/config.hh
@@ -0,0 +1,6 @@
+#ifndef CONFIG_HH
+#define CONFIG_HH
+
+#define LOCALE "en"
+
+#endif
---
+++ src/translations.hh
@@ -0,0 +1,6 @@
+#ifndef TRANSLATIONS_HH
+#define TRANSLATIONS_HH
+
+#define LIMIT_STR "{\"limit\":{\"en\":\"Limit\",\"it\":\"Limite\"}}"
+
+#endif
